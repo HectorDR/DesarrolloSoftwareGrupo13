dab6b255521ce2386a27e0d2480a439ecaab85eb
HectorDR <delgado.h.a@gmail.com>
2021-10-06 16:55:09 -0400

    ramas sprint2

---
+++ DesarrolloSoftwareGrupo13-feature-HU_005_Perez_Login/Informacion Usuario/js/sweetAlert.js
@@ -0,0 +1,9 @@
+
+Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'La informacion de usuario se a guardado correctamente',
+    showConfirmButton: false,
+    confirmButtonText: "seleccionar",
+    timer: 3500
+       })
